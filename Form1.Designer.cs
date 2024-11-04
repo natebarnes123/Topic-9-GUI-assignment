4a169559042dce60fc7a0bd4e668ac4022d67097
Nathan Barnes <339723215@GoTVDSB.ca>
2024-11-04 09:39:38 -0500

    jk

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGood = new System.Windows.Forms.Button();
+            this.btnBad = new System.Windows.Forms.Button();
+            this.imgGood = new System.Windows.Forms.PictureBox();
+            this.imgBad = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGood)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGood
+            // 
+            this.btnGood.Location = new System.Drawing.Point(84, 109);
+            this.btnGood.Name = "btnGood";
+            this.btnGood.Size = new System.Drawing.Size(97, 29);
+            this.btnGood.TabIndex = 0;
+            this.btnGood.Text = "Good";
+            this.btnGood.UseVisualStyleBackColor = true;
+            this.btnGood.Click += new System.EventHandler(this.btnGood_Click);
+            // 
+            // btnBad
+            // 
+            this.btnBad.Location = new System.Drawing.Point(220, 109);
+            this.btnBad.Name = "btnBad";
+            this.btnBad.Size = new System.Drawing.Size(103, 29);
+            this.btnBad.TabIndex = 1;
+            this.btnBad.Text = "Bad";
+            this.btnBad.UseVisualStyleBackColor = true;
+            this.btnBad.Click += new System.EventHandler(this.btnBad_Click);
+            // 
+            // imgGood
+            // 
+            this.imgGood.Image = ((System.Drawing.Image)(resources.GetObject("imgGood.Image")));
+            this.imgGood.Location = new System.Drawing.Point(109, 144);
+            this.imgGood.Name = "imgGood";
+            this.imgGood.Size = new System.Drawing.Size(200, 199);
+            this.imgGood.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgGood.TabIndex = 2;
+            this.imgGood.TabStop = false;
+            this.imgGood.Visible = false;
+            this.imgGood.Click += new System.EventHandler(this.imgGood_Click);
+            // 
+            // imgBad
+            // 
+            this.imgBad.Image = ((System.Drawing.Image)(resources.GetObject("imgBad.Image")));
+            this.imgBad.Location = new System.Drawing.Point(109, 144);
+            this.imgBad.Name = "imgBad";
+            this.imgBad.Size = new System.Drawing.Size(196, 199);
+            this.imgBad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgBad.TabIndex = 3;
+            this.imgBad.TabStop = false;
+            this.imgBad.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imgBad);
+            this.Controls.Add(this.imgGood);
+            this.Controls.Add(this.btnBad);
+            this.Controls.Add(this.btnGood);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgGood)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBad)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGood;
+        private System.Windows.Forms.Button btnBad;
+        private System.Windows.Forms.PictureBox imgGood;
+        private System.Windows.Forms.PictureBox imgBad;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,39 @@ namespace Topic_9_GUI_assignment
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGood_Click(object sender, EventArgs e)
+        {
+            btnGood.Enabled = true;
+
+            if (imgGood.Visible)
+            {
+                imgGood.Visible = false;
+            }
+            else
+            {
+                imgGood.Visible = true;
+            }
+        }
+
+        private void btnBad_Click(object sender, EventArgs e)
+        {
+            btnBad.Enabled = true; 
+
+            if (imgBad.Visible)
+            {
+                imgBad.Visible = false;
+            }
+            else
+            {
+                imgBad.Visible = true;
+            }
+        }
+
+        private void imgGood_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
